94a12b53ff18c2f6a3e5cfabbe886756051da230
Jan Rabe <jan.rabe@kibotu.net>
2018-08-28 13:55:51 +0200

    Update iFrame.js
    added refresh interval

--- iFrame.js
+++ iFrame.js
@@ -8,21 +8,33 @@
  */
 
 Module.register("iFrame",{
-		// Default module config.
-		defaults: {
-				height:"300px",
-				width:"100%"
-		},
 
+    // Default module config.
+    defaults: {
+            height:"300px",
+            width:"100%",
+            refreshInterval: 60000
+    },
+
+    start: function() {
+        if (this.config.refreshInterval <= 0) {
+            return;
+        }
+
+        var self = this;
+        setInterval(function() {
+                document.getElementById('fancy-iframe').src += '';
+        }, this.config.refreshInterval);
+    },
 
 	// Override dom generator.
 	getDom: function() {
-		var iframe = document.createElement("IFRAME");
+		var iframe = document.createElement("iframe");
 		iframe.style = "border:0"
+		iframe.id = 'fancy-iframe';
 		iframe.width = this.config.width;
 		iframe.height = this.config.height;
 		iframe.src =  this.config.url;
 		return iframe;
 	}
-
 });
